1afb01f6bbb9d5a916ec31e73f42d64d84c25537
Laszlo Papp <laszlo.papp@foundry.com>
2021-12-04 09:11:09 +0000

    Add merge sorted array

--- arrays-101/merge-sorted-array.cpp
+++ arrays-101/merge-sorted-array.cpp
@@ -6,7 +6,10 @@ using namespace std;
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        
+      while (n > 0) {
+        if (!m or nums1[m - 1] <= nums2[n - 1]) { nums1[m + n - 1] = nums2[n - 1]; --n; }
+        else { nums1[m + n - 1] = nums1[m - 1]; --m; };
+      }
     }
 };
 
@@ -22,19 +25,18 @@ int main()
 {
   vector<int> M1{1, 2, 3, 0, 0, 0};
   vector<int> N1{2, 5, 6};
-  cout << "[1,2,3,0,0,0] | [2,5,6] => [1,2,2,3,5,6]: ";
+  cout << "[1,2,3,0,0,0] | 3 | [2,5,6] | 3 => [1,2,2,3,5,6]: ";
   test(M1, 3, N1, 3);
 
   vector<int> M2{1};
   vector<int> N2{};
-  cout << "[1] | [] => [1]: ";
+  cout << "[1] 1 | [] | 0 => [1]: ";
   test(M2, 1, N2, 0);
 
   vector<int> M3{0};
   vector<int> N3{1};
-  cout << "[0] | [1] => [1]: ";
+  cout << "[0] | 0 | [1] | 1 => [1]: ";
   test(M3, 0, N3, 1);
 
-  return 0;
   return 0;
 }
