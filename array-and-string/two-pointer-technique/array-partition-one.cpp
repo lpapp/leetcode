2e3e1d75bf87a0cd731e6976027e31fd3f9cbd8e
Laszlo Papp <laszlo.papp@foundry.com>
2021-12-07 18:17:52 +0000

    Add hashed array partition one

--- array-and-string/two-pointer-technique/array-partition-one.cpp
+++ array-and-string/two-pointer-technique/array-partition-one.cpp
@@ -5,10 +5,22 @@ using namespace std;
 
 class Solution {
 public:
-    int arrayPairSum(vector<int>& nums) {
+    int arrayPairSum_sort(vector<int>& nums) {
       sort(nums.begin(), nums.end());
       int sum = 0;
-      for (size_t i = 0; i < nums.size(); ++i) if (!(i&1)) sum += nums[i];
+      for (size_t i = 0; i < nums.size(); i += 2) sum += nums[i];
+      return sum;
+    }
+
+    int arrayPairSum(vector<int>& nums) {
+      vector<int> nums_hash(20001, 0);
+      for (const int e : nums) ++nums_hash[e + 10000];
+      int sum = 0;
+      bool minimum = true;
+      for (size_t i = 0; i < nums_hash.size();) {
+        if (nums_hash[i]) { if (minimum) sum += i - 10000; minimum = !minimum; --nums_hash[i]; }
+        else ++i;
+      }
       return sum;
     }
 };
