90ff1af893c92d87ef5ef64b6478cc58b56ab8f1
Laszlo Papp <lpapp@amazon.com>
2025-07-19 14:04:12 +0100

    Add solution for Remove Duplicates From an Unsorted Linked List
    1836. Remove Duplicates From an Unsorted Linked List
    https://leetcode.com/problems/remove-duplicates-from-an-unsorted-linked-list/

---
+++ misc/1836-remove-duplicates-from-an-unsorted-linked-list/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+
+struct LinkedListNode
+{
+    int data;
+    LinkedListNode* next;
+};
+
+class Solution {
+public:
+    void deleteDups(LinkedListNode* node) {
+        unordered_set<int> nodeSet;
+        for (LinkedListNode* previous = nullptr; node; node = node->next) {
+            if (nodeSet.contains(node->data)) previous->next = node->next;
+            else { nodeSet.insert(node->data); previous = node; }
+        }
+    }
+};
+
+int main()
+{
+    Solution s;
+    LinkedListNode node;
+    node.data = 0;
+    node.next = nullptr;
+    s.deleteDups(&node);
+    return 0;
+}
