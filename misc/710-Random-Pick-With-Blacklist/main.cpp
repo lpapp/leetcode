4f5d68ed43dc767aaff6b6bfb59beb6cdbf5ab98
Laszlo Papp <lpapp@amazon.com>
2025-08-03 07:28:20 +0100

    Rearrange files

--- misc/710-Random-Pick-With-Blacklist/main.cpp
+++ misc/710-Random-Pick-With-Blacklist/main.cpp
@@ -13,16 +13,16 @@ using namespace std;
 
 class Solution {
     int allowlistSize;
-    std::unordered_map<int, int> blocklistHash;
+    unordered_map<int, int> blocklistHash;
 
 public:
-    Solution(int n, std::vector<int>& blocklist) {
-        allowlistSize = n - blocklist.size();
+    Solution(int n, vector<int>& blacklist) {
+        allowlistSize = n - blacklist.size();
         int mapIndex = allowlistSize;
-        const std::unordered_set<int> blocklistSet(blocklist.cbegin(), blocklist.cend());
-	// Map the excluded items to included items outside of the N - B range,
-	// where B is the size os the block list.
-        for (const int blockNumber : blocklist) {
+        const unordered_set<int> blocklistSet(blacklist.cbegin(), blacklist.cend());
+	    // Map the excluded items to included items outside of the N - B range,
+	    // where B is the size os the block list.
+        for (const int blockNumber : blacklist) {
             if (blockNumber < allowlistSize) {
                 while (blocklistSet.contains(mapIndex)) ++mapIndex;
                 blocklistHash[blockNumber] = mapIndex++;
@@ -31,17 +31,16 @@ public:
     }
 
     int pick() {
-        std::srand(std::time(nullptr));
-        const int randomNumber = std::rand() % allowlistSize;
+        const int randomNumber = rand() % allowlistSize;
         return blocklistHash.contains(randomNumber) ? blocklistHash[randomNumber] : randomNumber;
     }
 };
 
 int main()
 {
-    std::vector<int> i1{2, 3, 5};
+    vector<int> i1{2, 3, 5};
     Solution* obj = new Solution(7, i1);
-    std::cout << obj->pick() << std::endl;
+    cout << obj->pick() << endl;
 
     return 0;
 }
