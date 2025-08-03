6980f2780970751ccd5591b8b60384f944e8d447
Laszlo Papp <lpapp@amazon.com>
2025-08-03 07:39:01 +0100

    Rearrange files

--- misc/1971-Find-if-Path-Exists-in-Graph/main.cpp
+++ misc/1971-Find-if-Path-Exists-in-Graph/main.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -11,24 +12,21 @@ using namespace std;
 // n: length of the string
 
 class Solution {
-  vector<int> parent;
+    vector<int> parent;
 public:
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
       parent.resize(n);
       // Initialise the root parent to itself.
-      for (int i = 0; i < n; ++i)
-        parent[i] = i;
+      for (int i = 0; i < n; ++i) parent[i] = i;
 
-      for (const vector<int>& edge : edges)
-        unionSet(find(edge[0]), find(edge[1]));
+      for (const vector<int>& edge : edges) unionSet(find(edge[0]), find(edge[1]));
 
       // If the root parents are the same, there is a valid path between them.
       return find(source) == find(destination);
     }
 
     int find(int x) {
-        if (parent[x] != x)
-          parent[x] = find(parent[x]);
+        if (parent[x] != x) parent[x] = find(parent[x]);
 
         return parent[x];
     }
@@ -37,29 +35,28 @@ public:
         int rootX = find(x);
         int rootY = find(y);
 
-        if (rootX != rootY)
-          parent[rootX] = rootY;       
+        if (rootX != rootY) parent[rootX] = rootY;       
     }
 };
 
 int main()
 {
   Solution solution;
-  std::vector<std::vector<int>> i1{
+  vector<vector<int>> i1{
     {0, 1},
     {1, 2},
     {2, 0},
   };
-  std::cout << solution.validPath(3, i1, 0, 2) << std::endl;
+  cout << solution.validPath(3, i1, 0, 2) << endl;
 
-  std::vector<std::vector<int>> i2{
+  vector<vector<int>> i2{
     {0, 1},
     {0, 2},
     {3, 5},
     {5, 4},
     {4, 3},
   };
-  std::cout << solution.validPath(6, i2, 0, 5) << std::endl;
+  cout << solution.validPath(6, i2, 0, 5) << endl;
 
   return 0;
 }
