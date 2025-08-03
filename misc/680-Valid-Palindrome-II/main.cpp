a8a7f90d9f4284db6984ee02e012a21987bf6266
Laszlo Papp <lpapp@amazon.com>
2025-08-03 07:35:54 +0100

    Rearrange files

--- misc/680-Valid-Palindrome-II/main.cpp
+++ misc/680-Valid-Palindrome-II/main.cpp
@@ -11,24 +11,22 @@ using namespace std;
 // n: length of the string
 
 class Solution {
-  static bool isPalindrome(const std::string& s, std::size_t left, std::size_t right) {
-    for (; left < right; ++left, --right) {
-      if (s[left] != s[right]) return false;
+    static bool isPalindrome(const std::string& s, std::size_t left, std::size_t right) {
+        for (; left < right; ++left, --right) {
+            if (s[left] != s[right]) return false;
+        }
+        
+        return true;
     }
-
-    return true;
-  }
 
 public:
     bool validPalindrome(string s) {
-      for (std::size_t left = 0, right = s.size() - 1; left < right; ++left, --right) {
-        if (s[left] != s[right]) {
-          return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+        for (std::size_t left = 0, right = s.size() - 1; left < right; ++left, --right) {
+            if (s[left] != s[right]) return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
         }
-     }
 
-     return true;
-  }
+        return true;
+    }
 };
 
 
