7367a82bfa10145628c86e8ce8c020654756f38e
Laszlo Papp <lpapp@amazon.com>
2026-05-27 09:05:00 +0100

    Solve Snakes and Ladders
    909. Snakes and Ladders
    https://leetcode.com/problems/snakes-and-ladders/description/

--- matrix/909-Snakes-and-Ladders/main1.cpp
+++ matrix/909-Snakes-and-Ladders/main1.cpp
@@ -1,4 +1,6 @@
+#include <algorithm>
 #include <cassert>
+#include <queue>
 #include <vector>
 
 using namespace std;
@@ -6,7 +8,28 @@ using namespace std;
 class Solution {
 public:
     int snakesAndLadders(vector<vector<int>>& board) {
-        
+        const int n = board.size(), totalCells = n * n;
+        vector<bool> visited(totalCells + 1, false);
+        visited[1] = true;
+        int res = 0;
+        for (queue<int> q({1}); !q.empty();) {
+            for (int i = 0, size = q.size(); i < size; ++i) {
+                int current = q.front(); q.pop();
+                if (current == totalCells) return res;
+                for (int next = current + 1; next <= min(current + 6, totalCells); ++next) {
+                    int row = (next - 1) / n, column = (next - 1) % n;
+                    if (row % 2 == 1) column = n - column - 1;
+                    row = n - row - 1;
+                    const int dest = (board[row][column] == -1) ? next : board[row][column];
+                    if (!visited[dest]) {
+                        visited[dest] = true;
+                        q.push(dest);
+                    }
+                }
+            }
+            ++res;
+        }
+        return -1;
     }
 };
 
@@ -14,17 +37,17 @@ int main()
 {
 	Solution s;
     vector<vector<int>> board1 = {
-        {-1, -1, -1, -1, -1, -1},
-        {-1, -1, -1, -1, -1, -1},
-        {-1, -1, -1, -1, -1, -1},
-        {-1, 35, -1, -1, 13, -1},
-        {-1, -1, -1, -1, -1, -1},
-        {-1, 15, -1, -1, -1, -1}
+		{-1, -1, -1, -1, -1, -1},
+		{-1, -1, -1, -1, -1, -1},
+		{-1, -1, -1, -1, -1, -1},
+		{-1, 35, -1, -1, 13, -1},
+		{-1, -1, -1, -1, -1, -1},
+		{-1, 15, -1, -1, -1, -1}
     };
     assert(s.snakesAndLadders(board1) == 4);
     vector<vector<int>> board2 = {
-        {-1, -1},
-        {-1, 3}
+		{-1, -1},
+		{-1, 3}
     };
     assert(s.snakesAndLadders(board2) == 1);
 	return 0;
