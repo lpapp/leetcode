9a46b833dcd0ef4c17f04799a6ad1ba5f9058d87
Laszlo Papp <lpapp@amazon.com>
2026-06-02 07:18:07 +0100

    Solve Find Median from Data Stream
    295. Find Median from Data Stream
    https://leetcode.com/problems/find-median-from-data-stream/description/?envType=study-plan-v2&envId=top-interview-150

---
+++ heap/295-Find-Median-from-Data-Stream/main1.cpp
@@ -0,0 +1,26 @@
+#include <cassert>
+#include <functional>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+class MedianFinder {
+    priority_queue<int> maxHeap;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+public:
+    MedianFinder() = default;
+    void addNum(int num) { maxHeap.push(num); minHeap.push(maxHeap.top()); maxHeap.pop(); if (minHeap.size() > maxHeap.size() + 1) { maxHeap.push(minHeap.top()); minHeap.pop(); } }
+    double findMedian() { return minHeap.size() == maxHeap.size() ? (minHeap.top() + maxHeap.top()) / 2.0 : minHeap.top();   }
+};
+
+int main()
+{
+    MedianFinder medianFinder;
+    medianFinder.addNum(1);
+    medianFinder.addNum(2);
+    assert(medianFinder.findMedian() == 1.5);
+    medianFinder.addNum(3);
+    assert(medianFinder.findMedian() == 2.0);
+    return 0;
+}
