4a8a6d060571a18b70ab855b7cdbcfd64c89747c
Laszlo Papp <lpapp@amazon.com>
2026-06-03 05:56:10 +0100

    Solve Single Number II
    137. Single Number II
    https://leetcode.com/problems/single-number-ii/description/?envType=study-plan-v2&envId=top-interview-150

--- bit-manipulation/137-Single-Number-II/main1.cpp
+++ bit-manipulation/137-Single-Number-II/main1.cpp
@@ -6,7 +6,13 @@ using namespace std;
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        
+        int res = 0;
+        for (int i = 0; i < 32; ++i) {
+            int count = 0;
+            for (int num : nums) count += ((num >> i) & 1);
+            res |= ((count % 3) << i);
+        }
+        return res;       
     }
 };
 
