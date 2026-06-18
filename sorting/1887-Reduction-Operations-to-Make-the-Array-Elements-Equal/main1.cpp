e7583eff69d6324c9b5a4bd0b4fdafcfb254f540
Laszlo Papp <lpapp@amazon.com>
2026-06-18 06:33:52 +0100

    Solve Reduction Operations to Make the Array Elements Equal
    1887. Reduction Operations to Make the Array Elements Equal
    https://leetcode.com/problems/reduction-operations-to-make-the-array-elements-equal/?envType=problem-list-v2&envId=dsa-sorting-plateau-sorting

--- sorting/1887-Reduction-Operations-to-Make-the-Array-Elements-Equal/main1.cpp
+++ sorting/1887-Reduction-Operations-to-Make-the-Array-Elements-Equal/main1.cpp
@@ -9,10 +9,7 @@ public:
     int reductionOperations(vector<int>& nums) {
         ranges::sort(nums);
         int res = 0;
-        for (int i = 1, n = nums.size(), operations = 0; i < n; ++i) {
-            if (nums[i] != nums[i - 1]) ++operations;
-            res += operations;
-        }
+        for (int i = 1, n = nums.size(), operations = 0; i < n; res += operations, ++i) operations += nums[i] != nums[i - 1];
         return res;
     }
 };
