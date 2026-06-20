b241a73a114144d31ecdaf1c27c1bea1aa519101
Laszlo Papp <lpapp@amazon.com>
2026-06-20 16:38:37 +0100

    Solve Create Grid With Exactly One Path
    3963. Create Grid With Exactly One Path
    https://leetcode.com/contest/biweekly-contest-185/problems/create-grid-with-exactly-one-path/description/

---
+++ binary-tree/106-Construct-Binary-Tree-from-Inorder-and-Postorder-Traversal/main1.cpp
@@ -0,0 +1,59 @@
+#include <cassert>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+
+class Solution {
+    int inIdx, postIdx;
+    TreeNode* build(vector<int>& inorder, vector<int>& postorder, int stop) {
+        if (postIdx < 0) return nullptr;
+        if (inorder[inIdx] == stop) { --inIdx; return nullptr; }
+        auto* node = new TreeNode(postorder[postIdx--]);
+        node->right = build(inorder, postorder, node->val);
+        node->left = build(inorder, postorder, stop);
+        return node;
+    }
+public:
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        inIdx = postIdx = inorder.size() - 1;
+        return build(inorder, postorder, INT_MIN);
+    }
+};
+
+int main()
+{
+    Solution s;
+    vector<int> inorder1 = {9, 3, 15, 20, 7};
+    vector<int> postorder1 = {9, 15, 7, 20, 3};
+    TreeNode* res1 = s.buildTree(inorder1, postorder1);
+    assert(res1->val == 3);
+    assert(res1->left->val == 9);
+    assert(!res1->left->left);
+    assert(!res1->left->right);
+    assert(res1->right->val == 20);
+    assert(res1->right->left->val == 15);
+    assert(!res1->right->left->left);
+    assert(!res1->right->left->right);
+    assert(res1->right->right->val == 7);
+    assert(!res1->right->right->left);
+    assert(!res1->right->right->right);
+
+    vector<int> inorder2 = {-1};
+    vector<int> postorder2 = {-1};
+    TreeNode* res2 = s.buildTree(inorder2, postorder2);
+    assert(res2->val == -1);
+    assert(!res2->left);
+    assert(!res2->right);
+	return 0;
+}
