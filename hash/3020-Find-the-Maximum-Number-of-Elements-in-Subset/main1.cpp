11fd0196bae3623ea0ab845b09d7cd95d590f3ed
Laszlo Papp <lpapp@amazon.com>
2026-06-27 06:58:13 +0100

    Fix bugs

--- hash/3020-Find-the-Maximum-Number-of-Elements-in-Subset/main1.cpp
+++ hash/3020-Find-the-Maximum-Number-of-Elements-in-Subset/main1.cpp
@@ -15,8 +15,8 @@ public:
     	for (const auto& [base, freq] : hash) {
     	    int len = 0;
     	    long long cur = base;
-    	    while (hash[cur] > 1) { len += 2; cur *= cur; }
-    	    len += hash[cur] ? 1 : -1;
+    	    while (hash.contains(cur) && hash[cur] > 1) { len += 2; cur *= cur; }
+    	    len += hash.contains(cur) ? 1 : -1;
     	    res = max(res, len);
     	}
     	return res;
@@ -30,5 +30,7 @@ int main()
     assert(s.maximumLength(nums1) == 3);
     vector<int> nums2 = {1, 3, 2, 4};
     assert(s.maximumLength(nums2) == 1);
+    vector<int> nums3 = {48841, 358801, 28561, 18974736, 4356, 221, 358801, 599, 13, 4356, 66, 48841, 28561, 815730721, 13, 815730721, 18974736, 66, 169, 599, 169, 221};
+    assert(s.maximumLength(nums3) == 7);
     return 0;
 }
