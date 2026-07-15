9f86f422c5106c926c633fd5e827f46ae8492de1
Laszlo Papp <lpapp@amazon.com>
2026-07-15 05:51:25 +0100

    Solve GCD of Odd and Even Sums
    3658. GCD of Odd and Even Sums
    https://leetcode.com/problems/gcd-of-odd-and-even-sums/?envType=daily-question&envId=2026-07-15

---
+++ math/3658-GCD-of-Odd-and-Even-Sums/main1.cpp
@@ -0,0 +1,16 @@
+#include <cassert>
+
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+};
+
+int main()
+{
+	Solution s;
+    assert(s.gcdOfOddEvenSums(4) == 4);
+    assert(s.gcdOfOddEvenSums(5) == 5);
+	return 0;
+}
