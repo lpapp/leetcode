fe9cc3924fabb0b2cfc84a20a9929347bbd9eb75
Laszlo Papp <lpapp@amazon.com>
2026-09-15 08:14:53 +0100

    Solve Maximum Number of Non-overlapping Palindrome Substrings
    2472. Maximum Number of Non-overlapping Palindrome Substrings
    https://leetcode.com/problems/maximum-number-of-non-overlapping-palindrome-substrings/?envType=daily-question&envId=2026-09-15

--- array-and-string/2472-Maximum-Number-of-Non-Overlapping-Palindrome-Substrings/main1.cpp
+++ array-and-string/2472-Maximum-Number-of-Non-Overlapping-Palindrome-Substrings/main1.cpp
@@ -1,39 +1,21 @@
-#include <iostream>
+#include <cassert>
 #include <string>
-#include <vector>
 
 using namespace std;
 
 class Solution {
-	int dfs(size_t start, int k, size_t sLength, vector<int>& memo, const vector<vector<bool>>& isPalindrome) {
-	    if (start >= sLength) return 0;
-	    if (memo[start] != -1) return memo[start];
-	    int maxCount = dfs(start + 1, k, sLength, memo, isPalindrome);
-	    for (size_t end = start + k - 1; end < sLength; ++end) {
-	        if (isPalindrome[start][end]) maxCount = max(maxCount, 1 + dfs(end + 1, k, sLength, memo, isPalindrome));
-	    }
-	    memo[start] = maxCount;
-	    return maxCount;
-	};
 public:
     int maxPalindromes(string s, int k) {
-        const size_t sLength = s.size();
-        vector<vector<bool>> isPalindrome(sLength, vector<bool>(sLength, true));
-        vector<int> memo(sLength, -1);
-        for (int left = sLength - 1; left >= 0; --left) {
-            for (size_t right = left + 1; right < sLength; ++right) {
-                isPalindrome[left][right] = (s[left] == s[right]) && 
-                                           isPalindrome[left + 1][right - 1];
-            }
-        }
-		return dfs(0, k, sLength, memo, isPalindrome);
+        int res = 0;
+        for (int center = 0, n = s.size(), start = 0; center < 2 * n - 1; ++center) { for (int l = center / 2, r = l + (center & 1); l >= start && r < n && s[l] == s[r]; --l, ++r) { if (r - l + 1 >= k) { ++res; start = r + 1; break; } } }
+        return res;
     }
 };
 
 int main()
 {
-	Solution s;
-    cout << s.maxPalindromes("abaccdbbd", 3) << endl;
-    cout << s.maxPalindromes("adbcda", 2) << endl;
-	return 0;
+    Solution s;
+    assert(s.maxPalindromes("abaccdbbd", 3) == 2);
+    assert(s.maxPalindromes("adbcda", 2) == 0);
+    return 0;
 }
