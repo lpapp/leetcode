afd06404f989dd2ae00788cb46b9139d31ab6c2e
Laszlo Papp <lpapp@amazon.com>
2026-09-17 07:07:40 +0100

    Improve the solution

--- queue/622-Design-Circular-Queue/main1.cpp
+++ queue/622-Design-Circular-Queue/main1.cpp
@@ -1,32 +1,32 @@
-#include <iostream>
+#include <cassert>
 #include <vector>
 
 using namespace std;
 
 class MyCircularQueue {
-    size_t front{0}, size{0}, capacity;
+    int front = 0, size = 0, capacity;
     vector<int> data;
 public:
-    MyCircularQueue(int k): capacity(k), data(vector<int>(k)) {}
+    MyCircularQueue(int k) : capacity(k), data(k) {}
     bool enQueue(int value) { if (isFull()) return false; data[(front + size++) % capacity] = value; return true; }
     bool deQueue() { if (isEmpty()) return false; front = (front + 1) % capacity; --size; return true; }
-    int Front() { return isEmpty() ? -1 : data[front]; }
-    int Rear() { return isEmpty() ? -1 : data[(front + size - 1) % capacity]; }
-    bool isEmpty() { return size == 0; }
-    bool isFull() { return size == capacity; }
+    int Front() const { return isEmpty() ? -1 : data[front]; }
+    int Rear() const { return isEmpty() ? -1 : data[(front + size - 1) % capacity]; }
+    bool isEmpty() const { return size == 0; }
+    bool isFull() const { return size == capacity; }
 };
 
 int main()
 {
-    MyCircularQueue* myCircularQueue = new MyCircularQueue(3);
-    cout << myCircularQueue->enQueue(1) << endl;
-    cout << myCircularQueue->enQueue(2) << endl;
-    cout << myCircularQueue->enQueue(3) << endl;
-    cout << myCircularQueue->enQueue(4) << endl;
-    cout << myCircularQueue->Rear() << endl;
-    cout << myCircularQueue->isFull() << endl;
-    cout << myCircularQueue->deQueue() << endl;
-    cout << myCircularQueue->enQueue(4) << endl;
-    cout << myCircularQueue->Rear() << endl;
+    MyCircularQueue myCircularQueue(3);
+    assert(myCircularQueue.enQueue(1));
+    assert(myCircularQueue.enQueue(2));
+    assert(myCircularQueue.enQueue(3));
+    assert(!myCircularQueue.enQueue(4));
+    assert(myCircularQueue.Rear() == 3);
+    assert(myCircularQueue.isFull());
+    assert(myCircularQueue.deQueue());
+    assert(myCircularQueue.enQueue(4));
+    assert(myCircularQueue.Rear() == 4);
     return 0;
 }
